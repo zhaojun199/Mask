9a4cdec8718436fac4bba40e7437280603f8a98d
赵俊 <527367156@qq.com>
2019-07-01 19:58:36 +0800

    增加错误监测

---
+++ src/core/apm/error/protectedEntryPoints.js
@@ -0,0 +1,17 @@
+import report from './report'
+
+// 在浏览器中这些代码入口抛出的错误并不是完整的Error对象，(在最新版Chrome中可以捕获到完整的Error对象)
+// 重写setTimeout等方法，收集完整的error错误
+function protectEntryPoint(fn) {
+    return function protectedFn() {
+        try {
+            return fn()
+        } catch (e) {
+            report.protected(e)
+        }
+    }
+}
+_oldSetTimeout = window.setTimeout
+window.setTimeout = function protectedSetTimeout(fn, time) {
+    return _oldSetTimeout.call(window, protectEntryPoint(fn), time)
+}
---
+++ src/core/apm/error/windowError.js
@@ -0,0 +1,15 @@
+import report from './report'
+
+// 捕获全局异常
+window.addEventListener('error', function(event) {
+	event.preventDefault() 	//	阻止error在控制台报错
+	// report to sever
+	report.jsError(event)
+})
+
+// 捕获promise异常
+window.addEventListener('unhandledrejection', function(event) {
+	event.preventDefault() 	//	阻止error在控制台报错
+	// report to sever
+	report.promiseError(event)
+})
